c8e4ded65249397715d93dfa31f57d811b20ffa1
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-07-05 06:59:01 +0600

    first page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     "./index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#9282C4",
+        "primary-1": "#532563",
+        secendry: "rgb(17,96,137)",
+        ternary: "#532563",
+        text: "rgb(102,102,102)",
+      },
+      fontFamily: {
+        roboto: ["Roboto"],
+      },
+    },
   },
   plugins: [],
 };
